b3907b2b971d289779f9b04d93e29193f441a5da
Gary Hou <gary.hou@nimblerx.com>
2017-07-24 15:53:53 -0700

    move TemperatureInput component up

--- src/App.js
+++ src/App.js
@@ -28,6 +28,37 @@ function tryConvert(temperature, convert) {
   return rounded.toString();
 }
 
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+// eslint-disable-next-line
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange}
+        />
+      </fieldset>
+    );
+  }
+}
+
 class Calculator extends Component {
   constructor(props) {
     super(props);
@@ -64,36 +95,6 @@ class Calculator extends Component {
           celsius={parseFloat(celsius)}
         />
       </div>
-    );
-  }
-}
-
-const scaleNames = {
-  c: 'Celsius',
-  f: 'Fahrenheit'
-};
-
-class TemperatureInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.props.onTemperatureChange(e.target.value);
-  }
-
-  render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
-    return (
-      <fieldset>
-        <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input
-          value={temperature}
-          onChange={this.handleChange}
-        />
-      </fieldset>
     );
   }
 }
